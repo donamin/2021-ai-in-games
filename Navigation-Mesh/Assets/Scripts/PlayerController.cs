3e58a6949c7e81b6d4b331c44a03f8f533483924
Amin Babadi <amin.babadi@aalto.fi>
2021-04-06 16:04:35 +0300

    added solution to the first 4 parts of the tutorial.

---
+++ Navigation-Mesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
