0086f12a717af66d0ddb40da5bdaf47b361e8b50
Amin Babadi <amin.babadi@aalto.fi>
2021-04-21 08:14:34 +0300

    Othello MCTS project added.

---
+++ Othello/Assets/Scripts/TileManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+    public Color tileColor;
+    private Board board;
+    private Point coord;
+
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        board = GameObject.Find("Board").GetComponent<Board>();
+        gameManager = GameObject.Find("Managers").GetComponent<GameManager>();
+	
+       
+    }
+
+    void Start()
+    {
+        tileColor = GetComponent<Renderer>().material.color;
+        board.state.Count++;
+
+        coord = new Point((board.state.Count - 1) /board.state.Size, (board.state.Count - 1) % board.state.Size);
+    }
+	
+    #region Mouse input methods
+    void OnMouseEnter()
+    {
+        if (!board.state.GameOver)
+        {
+            // Highlights the tile as yellow
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // Sets the tile back to its original color
+        GetComponent<Renderer>().material.color = tileColor;
+    }
+
+	public void setColour(Color c)
+	{
+		tileColor = c;
+		GetComponent<Renderer>().material.color = tileColor;
+	}
+
+    void OnMouseUpAsButton()
+    {
+		if (board.state.turn == BoardState.GameTurn.Player && !board.state.GameOver)
+        {
+            gameManager.SetInput(coord);
+        }
+    }
+    #endregion Mouse input methods
+}
