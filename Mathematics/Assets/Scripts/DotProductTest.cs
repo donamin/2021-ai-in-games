b14c8e10b124122ffe0ddbcc7f01649161ca5699
Amin Babadi <amin.babadi@aalto.fi>
2021-04-28 08:06:54 +0300

    Mathematics project added.

---
+++ Mathematics/Assets/Scripts/DotProductTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotProductTest : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 25, Color.green);
+        Debug.DrawLine(transform.position, enemy.transform.position, Color.red);
+        Vector3 a = transform.forward;
+        Vector3 b = enemy.transform.position - transform.position;
+        float dot = a.x * b.x + a.z * b.z;
+        //print(dot);
+        float angle = Mathf.Acos(dot / (a.magnitude * b.magnitude)) * Mathf.Rad2Deg;
+        if (Vector3.Dot(b, transform.right) < 0)
+            angle *= -1;
+        print(angle);
+    }
+}
---
+++ Mathematics/Assets/Scripts/MathTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(Vector3.zero, transform.position);
+        float length = Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.y, 2) + Mathf.Pow(transform.position.z, 2));
+        //Vector3.Distance(Vector3.zero, transform.position);
+        //transform.position.magnitude;
+        //transform.position.sqrMagnitude;
+        Debug.Log(length);
+        //if(transform.position.magnitude < 2)
+        if (transform.position.sqrMagnitude < 4)
+        {
+        }
+    }
+}
